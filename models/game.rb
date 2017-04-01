02960257ac521e115d4fc66ccbaba92b8f211165
chanjman <mariozugaj@gmail.com>
2017-04-01 19:32:30 +0200

    Add used letters in json for ajax response

--- models/game.rb
+++ models/game.rb
@@ -54,6 +54,7 @@ class Game
       guessed_letters: guessed_letters,
       remaining_moves: remaining_moves,
       secret_word: secret_word,
+      used_letters: used_letters,
       win: win?,
       lost: lost?
     }
