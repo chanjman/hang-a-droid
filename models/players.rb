85a0750e20c955f961c978b2f8957ceb9a934ee4
chanjman <mariozugaj@gmail.com>
2017-08-30 09:52:32 +0200

    Cleaning up

--- models/players.rb
+++ models/players.rb
@@ -6,20 +6,3 @@ class Player
     @name = name
   end
 end
-
-# AI that plays hangman
-class AI
-  attr_reader :knowledge, :name
-  attr_accessor :counter
-
-  def initialize(word_size)
-    @knowledge = DictionaryGraph.new.letter_occ_in_words(word_size)
-    @name = 'AI'
-    @counter = -1
-  end
-
-  def guess
-    @counter += 1
-    knowledge[counter]
-  end
-end
